087caf21e70fe3e562b75831e855f3b303d05739
Derek Bracken <derekbracken@gmail.com>
2021-10-12 11:13:13 +0100

    Added department class

--- src/main/java/com/example/employeeRelationship/employeeController/ProjectController.java
+++ src/main/java/com/example/employeeRelationship/employeeController/ProjectController.java
@@ -1,4 +1,29 @@
 package com.example.employeeRelationship.employeeController;
 
+
+import com.example.employeeRelationship.models.Project;
+import com.example.employeeRelationship.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping(value="/projects")
+    public List<Project> getAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    @GetMapping(value="/projects/{id}")
+    public Optional<Project> getProject(@PathVariable Long id){
+        return projectRepository.findById(id);
+    }
 }
--- src/test/java/com/example/employeeRelationship/EmployeeRelationshipApplicationTests.java
+++ src/test/java/com/example/employeeRelationship/EmployeeRelationshipApplicationTests.java
@@ -1,13 +1,52 @@
 package com.example.employeeRelationship;
 
+import com.example.employeeRelationship.models.Employee;
+import com.example.employeeRelationship.models.Project;
+import com.example.employeeRelationship.repositories.EmployeeRepository;
+import com.example.employeeRelationship.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class EmployeeRelationshipApplicationTests {
+class EmployeeserviceApplicationTests {
+
+//	Employee john;
+//	Project rain;
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+	@Autowired
+	ProjectRepository projectRepository;
+
+//	@Before
+//	public void before(){
+//
+//
+//	}
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void createEmployee(){
+		Employee john = new Employee("John", "Smith", "2079452");
+		employeeRepository.save(john);
+	}
+
+	@Test
+	public void createProject(){}
+
+//	@Test
+//	public void testStuff(){
+//		Project rain = new Project("Rain", 5);
+//		john.addProject(rain);
+//		rain.addEmployees(john);
+//		projectRepository.save(rain);
+//	}
 
 }
